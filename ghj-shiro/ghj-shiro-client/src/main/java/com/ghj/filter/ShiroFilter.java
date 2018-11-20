7c757c0c69a0fff6c57350269a3aacaf3d42cfa1
wanghui <1515764144@qq.com>
2018-11-20 22:40:57 +0800

    sso1.0

---
+++ ghj-shiro/ghj-shiro-client/src/main/java/com/ghj/filter/ShiroFilter.java
@@ -0,0 +1,18 @@
+package com.ghj.filter;
+
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.web.filter.authz.AuthorizationFilter;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class ShiroFilter extends AuthorizationFilter {
+    @Override
+    protected boolean isAccessAllowed(ServletRequest servletRequest, ServletResponse servletResponse, Object o) throws Exception {
+        Subject subject = getSubject(servletRequest, servletResponse);
+        Session session = subject.getSession();
+
+        return false;
+    }
+}
--- ghj-shiro/src/main/java/com/ghj/service/ShiroService.java
+++
@@ -1,9 +0,0 @@
-package com.ghj.service;
-
-import com.ghj.core.dto.UserDTO;
-import com.ghj.core.vo.UserVO;
-import org.springframework.stereotype.Service;
-
-@Service
-public class ShiroService extends AbstractShiroService<UserVO,UserDTO> {
-}
